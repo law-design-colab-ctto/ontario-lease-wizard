f26de67e5743e8babc51a2b1d600b70bb547df48
Matilde Park <matilde@park.computer>
2019-06-09 12:09:55 -0400

    Now we're routing!

--- src/components/content.js
+++ src/components/content.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Home from '../pages/home.js'
+import { Route } from 'react-router-dom'
 
 export class Content extends Component {
     render() {
         return (
             <div className="content">
-
+            <div className="contentRoute">
+                <Route exact path="/" component={Home} />
+            </div>
             <div className="sidebar">
                 
             </div>    
