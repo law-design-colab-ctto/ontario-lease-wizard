26fb794530a4db38b6ff383c19789598ccba8eb3
Matilde Park <matilde@park.computer>
2019-06-12 23:56:17 -0400

    Left header responsively scales better.

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,10 @@ export class Header extends Component {
     render() {
         return (
             <header>
-            <Home className="home" size={40} color="white" strokeWidth="1"/> <h1>Ontario Lease Wizard</h1>
+            <div className="homeHeader">
+                <Home className="home" size={40} color="white" strokeWidth="1" /> 
+                <h1>Ontario Lease Wizard</h1>
+            </div>
             <div className="sub-nav">
                     <li className="user"><User className="user" size={30} strokeWidth="1" color="white" /><span className="user">Profile</span></li>
                     <li className="settings"><Settings className="settings" size={30} strokeWidth="1" color="white" /><span className="settings">Settings</span></li>
