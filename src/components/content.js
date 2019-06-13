a7e62c3df77eebd9076723e1e6159616748764dc
Matilde Park <matilde@park.computer>
2019-06-12 22:18:19 -0400

    Preparing another component.

--- src/components/content.js
+++ src/components/content.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
-import Home from '../pages/home.js'
-import sidebarHome from '../sidebars/home.js'
 import { Route } from 'react-router-dom'
+
+// Importing our content pages.
+import Home from '../pages/home.js'
+import loginProfile from '../pages/login_start.js'
+
+// Importing our sidebars (they change with the route, as the pages do.)
+import sidebarHome from '../sidebars/home.js'
+
 
 export class Content extends Component {
     render() {
@@ -9,6 +15,7 @@ export class Content extends Component {
             <div className="content">
             <div className="contentRoute">
                 <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={loginProfile}/>
             </div>
             <div className="sidebar">
                 <h2>Learn More</h2>
---
+++ src/pages/login_start.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class loginProfile extends Component {
+    render() {
+        return (
+            <div>
+                <p>It's a login page.</p>
+            </div>
+        )
+    }
+}
+
+export default loginProfile
