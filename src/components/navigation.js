b3ec819a7daec9c125a57664e029a76e4bb5555d
Matilde Park <matilde@park.computer>
2019-06-15 14:50:02 -0400

    Migrating the sidebar expander code to nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -8,19 +8,60 @@ let moneySvg = require("../assets/navigation/money.svg"),
     partiesSvg = require("../assets/navigation/parties.svg"),
     additionalTermsSvg = require("../assets/navigation/additional terms.svg")
 
+class NavigationItem extends Component {
+    constructor(props) {
+        super(props)
+        this.toggleClass = this.toggleClass.bind(this)
+        this.state = {
+            active: false
+        }
+    }
+    toggleClass() {
+        const currentState = this.state.active
+        this.setState({ active: !currentState })
+    }
+    render() {
+        let divStyle = {
+            hidden: {
+                visibility: "hidden",
+                height: 0,
+                opacity: 0
+            },
+            visible: {
+                visibility: "visible",
+                height: "auto",
+                opacity: 1
+            }
+        }
+        return (
+            <li
+                className={this.state.active ? 'active' : null}
+                onClick={this.toggleClass}
+            >
+            <ChevronRight className={"chevronRight " + (this.state.active ? 'active' : null)} side={25}/> 
+            <img src={this.props.imageSrc} className={this.props.imageClass} alt={this.props.imageAlt} />
+            <span>{this.props.name}</span>
+            <div style={this.state.active ? divStyle.visible : divStyle.hidden}>
+                {this.props.detail}
+            </div>
+            </li>
+        )
+    }
+}
+
 export class navigation extends Component {
     render() {
         return (
             <nav>
-            <div>
+            <div className="navDiv">
                 <h2>Table of Contents</h2>
                 <ul>
-                    <li><ChevronRight className="chevronRight" size={25} /> <img src={moneySvg} className="Money" alt="Money" /> <span>Money</span></li>
-                    <li><ChevronRight className="chevronRight" size={25} /> <img src={dayToDaySvg} className="dayToDay" alt="Day to Day" /> <span>Day to Day</span></li>
-                    <li><ChevronRight className="chevronRight" size={25} /> <img src={timelineSvg} className="Timeline" alt="Timeline" /> <span>Timeline</span></li>
-                    <li><ChevronRight className="chevronRight" size={25} /> <img src={unitSvg} className="Unit" alt="Unit" /> <span>Unit</span></li>
-                    <li><ChevronRight className="chevronRight" size={25} /> <img src={partiesSvg} className="Parties" alt="Parties" /> <span>Parties</span></li>
-                    <li><ChevronRight className="chevronRight" size={25} /> <img src={additionalTermsSvg} className="AdditionalTerms" alt="Additional Terms" /> <span>Additional Terms</span></li>
+                    <NavigationItem imageSrc={moneySvg} imageClass="Money" imageAlt="Money" name="Money"/>
+                    <NavigationItem imageSrc={dayToDaySvg} imageClass="dayToDay" imageAlt="Day to Day" name="Day to Day"/>
+                    <NavigationItem imageSrc={timelineSvg} imageClass="Timeline" alt="Timeline" name="Timeline"/>
+                    <NavigationItem imageSrc={unitSvg} imageClass="Unit" imageAlt="Unit" name="Unit"/>
+                    <NavigationItem imageSrc={partiesSvg} imageClass="Parties" imageAlt="Parties" name="Parties"/>
+                    <NavigationItem imageSrc={additionalTermsSvg} imageclass="AdditionalTerms" imageAlt="Additional Terms" name="Additional Terms"/>
                 </ul>
             </div>
             </nav>
