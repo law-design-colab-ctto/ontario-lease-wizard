dcf82dbe60490fddced8254d9212099764d0c859
Matilde Park <matilde@park.computer>
2019-06-23 11:36:58 -0400

    Clicking the label checks the checkbox

--- src/pages/disclaimer.js
+++ src/pages/disclaimer.js
@@ -14,7 +14,7 @@ export class Disclaimer extends Component {
                 <p>Legal Disclaimer: this tool does not provide legal advice and does not guarantee compliance with the Residential Tenancies Act, 2006.</p>
                 <p>The information you enter in this wizard will be strictly protected. Please read our Privacy Policy. </p>
                 <p>By clicking “I agree”, you agree with the terms of our Privacy Policy and agree not to hold us liable for any dipsutes arising from the lease you create or review using this tool.</p>
-                <p><input type="checkbox" checked={this.state.checked} onChange={() => { this.setState({ checked: !this.state.checked }); }} /> I agree</p>
+                <p><label for="iagree"><input id="iagree" type="checkbox" checked={this.state.checked} onChange={() => { this.setState({ checked: !this.state.checked }); }} /> I agree</label></p>
                 <button disabled={!this.state.checked}>Next</button>
             </div>
         )
