257db44b20f117be83b4ed307eb77f9782e064e6
Matilde Park <matilde@park.computer>
2019-06-30 12:44:04 -0400

    Programmatic route buttons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 
 export class Footer extends Component {
     render() {
+        // It will only show our buttons if it's the right route for them to appear.
+        const { location } = this.props;
+        const previousHidden = (location) => {
+            switch(location.pathname) {
+                default: 
+                    return true
+            }
+        }
+        const nextHidden = (location) => {
+            switch(location.pathname) {
+                case '/concerns': 
+                    return false;
+                default: 
+                    return true
+            }
+        }
         return (
             <footer>
-                
+                <button id="previous" className={previousHidden(location) ? "hidden" : null}>Previous</button>  
+                <button id="next" className={nextHidden(location) ? "hidden" : null}>Next</button>              
             </footer>
         )
     }
 }
 
-export default Footer
+export default withRouter(Footer)
