f059bd11c4f1e662da9c5b00a7514247887db756
Matilde Park <matilde@park.computer>
2019-06-30 11:40:55 -0400

    Check out the pretty transitions.

--- src/components/content.js
+++ src/components/content.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import { AnimatedSwitch } from 'react-router-transition'
 
 // Importing our content pages.
 import Home from '../pages/home.js'
@@ -16,10 +17,21 @@ export class Content extends Component {
         return (
             <div className="content">
             <div className="contentRoute">
+                    <AnimatedSwitch
+                        atEnter={{ opacity: 0, offset: 100 }}
+                        atLeave={{ opacity: 0 }}
+                        atActive={{ opacity: 1, offset: 0 }}
+                        className="switch-wrapper"
+                        mapStyles={(styles) => ({
+                            transform: `translateX(${styles.offset}%)`,
+                            opacity: `${styles.opacity}`
+                        })}
+                    >
                 <Route exact path="/" component={Home} />
                 <Route exact path="/login" component={loginProfile}/>
                 <Route exact path="/disclaimer" component={Disclaimer}/>
                 <Route exact path="/concerns" render={() => <Concerns state={this.props.state} handler={this.props.handler} />}/>
+                </AnimatedSwitch>
             </div>
             <div className="sidebar">
                 <h2>Learn More</h2>
--- src/pages/disclaimer.js
+++ src/pages/disclaimer.js
@@ -15,7 +15,7 @@ export class Disclaimer extends Component {
                 <p>Legal Disclaimer: this tool does not provide legal advice and does not guarantee compliance with the Residential Tenancies Act, 2006.</p>
                 <p>The information you enter in this wizard will be strictly protected. Please read our Privacy Policy. </p>
                 <p>By clicking “I agree”, you agree with the terms of our Privacy Policy and agree not to hold us liable for any disputes arising from the lease you create or review using this tool.</p>
-                <p><label for="iagree"><input id="iagree" type="checkbox" checked={this.state.checked} onChange={() => { this.setState({ checked: !this.state.checked }); }} /> I agree</label></p>
+                <p><label htmlFor="iagree"><input id="iagree" type="checkbox" checked={this.state.checked} onChange={() => { this.setState({ checked: !this.state.checked }); }} /> I agree</label></p>
                 <Link className={this.state.checked ? null : 'disabledLink'} to="/concerns"><button disabled={!this.state.checked}>Next</button></Link>
             </div>
         )
